e209732aa60a93f37d53a34a28c19db04e0a32d3
SZaboAnna21 <szabo.anni12@gmail.com>
2023-02-07 21:37:53 +0100

    első commit-om

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rock-Paper-Scissor-game-
+## elsö git commit
---
+++ pockpapers.py
@@ -0,0 +1,68 @@
+#rock paper sicisors 
+import time
+import random
+
+print("Hello this is a Rock Paper Scissors game")
+
+def IsThePlayerReady():
+    valid_answer =["yes", "no", "Yes", "No"]
+    valid= False
+    while valid == False:
+        answer = input("Do you want to play ( yes or no)  ")
+        if answer not in valid_answer :
+            print("Please choose form yes or no")
+        elif answer == "no" or answer =="No":
+            print("thank you for playing! ")
+            exit()
+        else:
+            valid = True
+            return True
+
+
+valid= IsThePlayerReady()
+
+while valid:
+    print('Rock, Paper, Scissors - Shoot!')
+    time.sleep(2)
+    valid_choises=["R", "P", "S"]
+    valid2=True
+    while valid2:
+        player_chiose = input("Choose your weapon! \n[R]ock], [P]aper, or [S]cissors:  ")
+        if (player_chiose in valid_choises) :
+            if player_chiose == "R":
+                print("You choose Rock")
+            elif player_chiose =="S":
+                print(" You choose Scissors")
+            elif player_chiose == "P":
+                print("You choose Paper")
+            valid2= False
+        else: print("Sorry but that is not valid choose form (R P S) ")
+        time.sleep(1)
+
+
+    opponet_choise = random.choice(valid_choises)
+    time.sleep(1)
+
+    if opponet_choise =="R":
+        print("I choose Rock")
+    elif opponet_choise == "s":
+        print("I choose Scissors")
+    else:
+        print("I choose Paper")
+
+    
+    if player_chiose == opponet_choise :
+        print("Its a tiee!")
+        valid= IsThePlayerReady()
+    elif player_chiose == "P" and opponet_choise =="S":
+        print("Scissors beats Paper, I win")
+        valid= IsThePlayerReady()
+    elif player_chiose == "S" and opponet_choise =="R":
+        print("Rock beats Scissors, I win")
+        valid= IsThePlayerReady()
+    elif player_chiose == "R" and opponet_choise =="P":
+        print("Paper beats Rock, I win")
+        valid= IsThePlayerReady()
+    else:
+        print("You win!!")
+        valid= IsThePlayerReady()
